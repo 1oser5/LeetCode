a4734f1d616e7b4674ac7bd9a046b1aef535699f
1oser5 <snoopy98@163.com>
2019-10-10 07:14:15 +0800

    2019.10.10

---
+++ 算法/hammingDistance.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   hammingDistance.py
+@Time    :   2019/10/09 22:04:07
+@Author  :   Xia
+@Version :   1.0
+@Contact :   snoopy98@163.com
+@License :   (C)Copyright 2019-2020, HB.Company
+@Desc    :   
+两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+
+给出两个整数 x 和 y，计算它们之间的汉明距离。
+
+注意：
+0 ≤ x, y < 231.
+
+示例:
+
+输入: x = 1, y = 4
+
+输出: 2
+
+解释:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+上面的箭头指出了对应二进制位不同的位置。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/hamming-distance
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+# here put the import lib
+def hammingDistance(x: int, y: int) -> int:
+    return bin(x^y).count('1')
+        
+if __name__ == '__main__':
+    print(hammingDistance(0,1))
