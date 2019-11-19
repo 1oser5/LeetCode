2cff4e8e1de0df817b021826e17685de94428935
1oser5 <snoopy98@163.com>
2019-11-19 21:06:28 +0800

    del jenkins

---
+++ 算法/isValid.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   isValid.py
+@Time    :   2019/11/19 20:48:28
+@Author  :   Xia
+@Version :   1.0
+@Contact :   snoopy98@163.com
+@License :   (C)Copyright 2019-2020, HB.Company
+@Desc    :   
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+# here put the import lib
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if s == '':
+            return True
+        stack = []
+        for i in s:
+            if i == '}':
+                stack.pop('{')
+if __name__ == '__main__':
+    pass
