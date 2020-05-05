edb906a8fed3f230ec423ecd6cda0649aef610c8
1oser5 <snoopy98@163.com>
2020-05-05 21:16:33 +0800

    荷兰国旗

---
+++ 二刷+题解/剑指offer/sortColors.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   sortColors.py
+@Time    :   2020/05/05 20:58:03
+@Author  :   Apus
+@Version :   1.0
+@Contact :   snoopy98@163.com
+@License :   (C)Copyright 2020-2021, HB.Company
+@Desc    :   None
+'''
+
+# here put the import lib
+
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        size = len(nums)
+        if size < 2:
+            return
+        zero, two = 0, size
+        for i in range(size):
+            if nums[i] == 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
+            elif nums[i] == 1:
+                continue
+            else:
+                nums[i], nums[two] = nums[two], nums[i]
+                two -= 1
+
+
+
+if __name__ == '__main__':
+    pass
