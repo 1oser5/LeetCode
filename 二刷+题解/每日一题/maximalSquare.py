8b9553bcf0c8a75ff978e5d46654f31786530c0b
1oser5 <snoopy98@163.com>
2020-05-08 09:10:34 +0800

    每日一题

---
+++ 二刷+题解/每日一题/maximalSquare.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   maximalSquare.py
+@Time    :   2020/05/08 08:25:23
+@Author  :   Apus
+@Version :   1.0
+@Contact :   snoopy98@163.com
+@License :   (C)Copyright 2020-2021, HB.Company
+@Desc    :
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+
+示例:
+
+输入:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximal-square
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+# here put the import lib
+from typing import List
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if int(matrix[i-1][j-1]) == 1:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + int(matrix[i-1][j-1])
+        return max([max(i) for i in dp])**2
+
+if __name__ == '__main__':
+    s = Solution()
+    # c =s.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
+    c =s.maximalSquare([["1","0","1","1","0","1"],["1","1","1","1","1","1"],["0","1","1","0","1","1"],["1","1","1","0","1","0"],["0","1","1","1","1","1"],["1","1","0","1","1","1"]])
+    # c =s.maximalSquare([["1"]])
+    print(c)
