18df3352a08df71124942e9c6bbe5d29b0c9e6c0
夏天乐 <1oser5@xiatianledeMacBook-Pro.local>
2020-09-16 08:54:52 +0800

    2020.9.16 每日一题

---
+++ 二刷+题解/每日一题/invertTree.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   invertTree.py
+@Time    :   2020/09/16 08:47:32
+@Author  :   Xia
+@Version :   1.0
+@Contact :   snoopy98@163.com
+@License :   (C)Copyright 2019-2020, HB.Company
+@Desc    :   None
+'''
+
+# here put the import lib
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+if __name__ == '__main__':
+    pass
